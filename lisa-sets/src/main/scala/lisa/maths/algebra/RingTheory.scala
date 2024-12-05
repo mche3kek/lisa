079a5f2081d5eef0ea653d1104ff2ebed1bd67ab
Myriam-Himmi <mhimmi@LAPTOP-EB704NQL>
2024-12-05 17:56:51 +0100

    multiplicative identity uniqueness proved

--- lisa-sets/src/main/scala/lisa/maths/algebra/RingTheory.scala
+++ lisa-sets/src/main/scala/lisa/maths/algebra/RingTheory.scala
@@ -3,7 +3,9 @@ package lisa.maths.algebra
 import lisa.maths.algebra.GroupTheory.*
 import lisa.maths.settheory.SetTheory.*
 import lisa.maths.settheory.functions.Functionals.* 
+import lisa.maths.Quantifiers.*
 import lisa.automation.kernel.CommonTactics.Definition
+import lisa.automation.kernel.CommonTactics.ExistenceAndUniqueness
 
 object RingTheory extends lisa.Main {
     // Operations
@@ -14,7 +16,7 @@ object RingTheory extends lisa.Main {
     private val x, y, z = variable
 
     // Identity elements
-    private val e = variable
+    private val e, f = variable
 
     // Group in the ring
     private val G = variable
@@ -41,8 +43,13 @@ object RingTheory extends lisa.Main {
     * [[associativityAxiom]], [[identityExistence]] and [[distributivityAxiom]].
     */
     val ring = DEF(G, +, *) --> group(G, +) /\ abelianGroup(G, +) /\ closedByProducts /\ associativityAxiom(G, *) 
-                                /\ identityExistence(G, *) /\ distributivityAxiom(G, *, +)
+                                /\ distributivityAxiom(G, *, +)
 
+    /**
+    * Ring with identity --- A ring with identity (G, +, *) is a ring containing an identity element under '*', satisfying [[identityExistence]].
+    */
+    val identityRing = DEF(G, +, *) --> ring(G, +, *) /\ identityExistence(G, *)
+    
     /**
     * Commutative ring --- A ring is said to be commutative if every element commutes under '*',
     * i.e. it satisfies [[commutativityAxiom]].
@@ -50,10 +57,10 @@ object RingTheory extends lisa.Main {
     val commutativeRing = DEF(G, +, *) --> ring(G, +, *) /\ commutativityAxiom(G, *)
 
     /**
-    * Identity uniqueness --- In a ring (G, +, *), an identity element is unique, i.e. if both `e + x = x + e = x` and
+    * Additive Identity uniqueness --- In a ring (G, +, *), an additive identity element is unique, i.e. if both `e + x = x + e = x` and
     * `f + x = x + f = x` for all `x`, then `e = f`.
     *
-    * This justifies calling `e` <i>the</i> identity element.
+    * This justifies calling `e` <i>the</i> additive identity element.
     */
     val additiveIdentityUniqueness = Theorem(ring(G, +, *) |- ∃!(e, isNeutral(e, G, +))) {
         assume(ring(G, +, *))
@@ -66,6 +73,38 @@ object RingTheory extends lisa.Main {
     val additiveInverseUniqueness = Theorem((ring(G, +, *), x ∈ G) |- ∃!(y, isInverse(y, x, G, +)) ){
         assume(ring(G, +, *), x ∈ G)
         have(thesis) by Tautology.from(ring.definition, inverseUniqueness of (G -> G, * -> +))
+    }
+
+    /**
+    * Multiplicative identity uniqueness --- In a ring with identity (G, +, *), a multiplicative identity element is unique, 
+    * i.e. if both `e * x = x * e = x` and `f * x = x * f = x` for all `x`, then `e = f`.
+    *
+    * This justifies calling `e` <i>the</i> multiplicative identity element.
+    */
+    val multiplicativeIdentityUniqueness = Theorem(identityRing(G, +, *) |- ∃!(e, isNeutral(e, G, *))) {
+        assume(identityRing(G, +, *))
+        val existence = have(identityRing(G, +, *) |- ∃(e, isNeutral(e, G, *))) by Tautology.from(identityRing.definition, identityExistence.definition)
+
+        val uniqueness = have((isNeutral(e, G, *), isNeutral(f, G, *)) |- (e === f)) subproof {
+            val membership = have(isNeutral(e, G, *) |- e ∈ G) by Tautology.from(isNeutral.definition)
+            assume(isNeutral(e, G, *))
+            assume(isNeutral(f, G, *))
+
+            // 1. e * f = f
+            have(∀(x, x ∈ G ==> ((op(e, *, x) === x) /\ (op(x, *, e) === x)))) by Tautology.from(isNeutral.definition)
+            thenHave(f ∈ G ==> ((op(e, *, f) === f) /\ (op(f, *, e) === f))) by InstantiateForall(f)
+            val neutrality = thenHave(f ∈ G |- ((op(e, *, f) === f) /\ (op(f, *, e) === f))) by Restate
+            have((op(e, *, f) === f) /\ (op(f, *, e) === f)) by Cut(membership of (e -> f), neutrality)
+            val firstEq = thenHave(op(e, *, f) === f) by Tautology
+
+            // 2. e * f = e
+            have((op(f, *, e) === e) /\ (op(e, *, f) === e)) by Cut(membership of (e -> e), neutrality of (e -> f, f -> e))
+            val secondEq = thenHave(e === op(e, *, f)) by Tautology
+
+            // 3. Conclude by transitivity
+            have(thesis) by Tautology.from(firstEq, secondEq, equalityTransitivity of (x -> e, y -> op(e, *, f), z -> f))
+        }
+        have(thesis) by ExistenceAndUniqueness(isNeutral(e, G, *))(existence, uniqueness)    
     }
     
 
