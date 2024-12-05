1f42c775ca03a80bdb9ba9a38916c3b77580b374
Myriam-Himmi <mhimmi@LAPTOP-EB704NQL>
2024-12-05 13:22:09 +0100

    commutativeRing, additive identity and inverse uniqueness

--- lisa-sets/src/main/scala/lisa/maths/algebra/RingTheory.scala
+++ lisa-sets/src/main/scala/lisa/maths/algebra/RingTheory.scala
@@ -3,6 +3,7 @@ package lisa.maths.algebra
 import lisa.maths.algebra.GroupTheory.*
 import lisa.maths.settheory.SetTheory.*
 import lisa.maths.settheory.functions.Functionals.* 
+import lisa.automation.kernel.CommonTactics.Definition
 
 object RingTheory extends lisa.Main {
     // Operations
@@ -11,6 +12,9 @@ object RingTheory extends lisa.Main {
 
     // Ring elements
     private val x, y, z = variable
+
+    // Identity elements
+    private val e = variable
 
     // Group in the ring
     private val G = variable
@@ -28,16 +32,41 @@ object RingTheory extends lisa.Main {
     * Distributivity --- `*,+` are distributive (in `R`) if `x * (y + z) = x * y + x * z 
     * and (x + y) * z = x * z + y * z ` for all `x, y, z` in `R`.
     */
-    private val distributivityAxiom = DEF(G, *, +) -->
+    private val distributivityAxiom = DEF(G, +, *) -->
         ∀(x, x ∈ G ==> ∀(y, y ∈ G ==> ∀(z, z ∈ G ==> ( (op(x,*,op(y,+,z)) === op(op(x,*,y),+,op(x,*,z))) 
                                                         /\ (op(op(x,+,y),*,z) === op(op(x,*,z),+,op(y,*,z)))))))
 
     /**
-    * Ring --- A ring (G, *, +) is a set along with a law of composition `*` and '+', satisfying [[abelianGroup]], [[closedByProducts]],
+    * Ring --- A ring (G, +, *) is a set along with a law of composition `*` and '+', satisfying [[abelianGroup]], [[closedByProducts]],
     * [[associativityAxiom]], [[identityExistence]] and [[distributivityAxiom]].
     */
-    val ring = DEF(G, *, +) --> abelianGroup(G, +) /\ closedByProducts /\ associativityAxiom(G, *) 
+    val ring = DEF(G, +, *) --> group(G, +) /\ abelianGroup(G, +) /\ closedByProducts /\ associativityAxiom(G, *) 
                                 /\ identityExistence(G, *) /\ distributivityAxiom(G, *, +)
+
+    /**
+    * Commutative ring --- A ring is said to be commutative if every element commutes under '*',
+    * i.e. it satisfies [[commutativityAxiom]].
+    */
+    val commutativeRing = DEF(G, +, *) --> ring(G, +, *) /\ commutativityAxiom(G, *)
+
+    /**
+    * Identity uniqueness --- In a ring (G, +, *), an identity element is unique, i.e. if both `e + x = x + e = x` and
+    * `f + x = x + f = x` for all `x`, then `e = f`.
+    *
+    * This justifies calling `e` <i>the</i> identity element.
+    */
+    val additiveIdentityUniqueness = Theorem(ring(G, +, *) |- ∃!(e, isNeutral(e, G, +))) {
+        assume(ring(G, +, *))
+        have(thesis) by Tautology.from(ring.definition, identityUniqueness of (G -> G, * -> +))
+    }
+
+    /**
+    * Theorem --- The additive inverse of an element `x` (i.e. `y` such that `x + y = y + x = e`) in `G` is unique.
+    */
+    val additiveInverseUniqueness = Theorem((ring(G, +, *), x ∈ G) |- ∃!(y, isInverse(y, x, G, +)) ){
+        assume(ring(G, +, *), x ∈ G)
+        have(thesis) by Tautology.from(ring.definition, inverseUniqueness of (G -> G, * -> +))
+    }
     
 
 }
